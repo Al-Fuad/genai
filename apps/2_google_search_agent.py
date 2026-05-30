135b5c7a96265565c16bbb53c0ec32132d22d85d
Al-Fuad <smalfuadnur@gmail.com>
2026-05-30 11:14:34 +0600

    Refactor Google search agent implementation and update requirements

--- apps/2_google_search_agent.py
+++ apps/2_google_search_agent.py
@@ -2,15 +2,19 @@ from dotenv import load_dotenv
 from langchain_community.utilities import GoogleSerperAPIWrapper
 from langchain_groq import ChatGroq
 from langchain.agents import create_agent
+from langgraph.checkpoint.memory import MemorySaver
 
 load_dotenv()
+
 model = ChatGroq(model='openai/gpt-oss-20b')
 search = GoogleSerperAPIWrapper()
+memory = MemorySaver()
 
 agent = create_agent(
     model=model,
     tools=[search.run,],
-    system_prompt='You are an agent and can search for any question on google'
+    system_prompt='You are an agent and can search for any question on google',
+    checkpointer=memory
 )
 
 while True:
@@ -19,12 +23,19 @@ while True:
         print('Good Bye')
         break
 
-    response = agent.invoke({
-        'messages': [
-            {
-                'role': 'user',
-                'content': query
+    response = agent.invoke(
+        {
+            'messages': [
+                {
+                    'role': 'user',
+                    'content': query
+                }
+            ]
+        },
+        {
+            'configurable': {
+                'thread_id': '1'
             }
-        ]
-    })
+        }
+    )
     print('AI: ', response['messages'][-1].content)
